ccb05e354c6136e7fe8e0a8836b096ae59d965f2
tika.kristiani <tika.kristiani@gmail.com>
2021-03-09 09:53:54 +0700

    switch screen with fragment

--- app/src/main/java/com/enigma/gold_pocket/MainActivity.kt
+++ app/src/main/java/com/enigma/gold_pocket/MainActivity.kt
@@ -4,36 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
-    private var counter = 0;
+    lateinit var buyFragment: BuyFragment
+    lateinit var sellFragment: SellFragment
+    lateinit var historyFragment: HistoryFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        incrementButton.setOnClickListener(this)
-        decrementButton.setOnClickListener(this)
-        resetButton.setOnClickListener(this)
+        buyProduct.setOnClickListener(this)
+        sellProduct.setOnClickListener(this)
+        historyProduct.setOnClickListener(this)
+        buyFragment = BuyFragment()
+        sellFragment = SellFragment()
+        historyFragment = HistoryFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, buyFragment).commit()
         //jika melakukan set view, dll sebelum melakukan set content view maka akan error
         //karena kita harus melakukan set layout sebelum  isi didalam layout
 //        nameTextView.text = "Tika Yesi Kristiani"
     }
 
     override fun onClick(v: View?) {
-        when(v){
-            incrementButton -> {
-                counter++
-            }
-            decrementButton -> {
-                counter--
-            }
-            resetButton ->{
-                counter=0
-            }
+    when(v){
+        buyProduct -> {
+            switchFragment(buyFragment)
         }
-        resultText.text = counter.toString()
+        sellProduct -> {
+            switchFragment(sellFragment)
+        }
+        historyProduct -> {
+            switchFragment(historyFragment)
+        }
+    }
+    }
+
+    fun switchFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 
 
